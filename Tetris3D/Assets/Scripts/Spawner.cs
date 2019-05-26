a9bebd27b5f40045c2df7b84eb8c46c9196fd92a
sommerlilie <rex.saarlooswolfhonden@t-online.de>
2019-05-26 16:53:10 +0200

    Added Spawner

--- Tetris3D/Assets/Scripts/Spawner.cs
+++ Tetris3D/Assets/Scripts/Spawner.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour {
-	GameObject[] forms;
-
-	public void SpawnNext() {
-    int i = Random.Range(0, forms.Length);
-
-    // Spawn Group at current Position
-    Instantiate(forms[i], transform.position, Quaternion.identity);
+	[SerializeField]
+	GameObject[] forms = {};
 
 	void Start() {
-	 forms = Resources.LoadAll("MyItemsToLoad") as GameObject[];
+//	 forms = Resources.LoadAll("MyItemsToLoad") as GameObject[];
+//	 Debug.Log(forms);
 	 SpawnNext();
 	}
-  }
+
+	public void SpawnNext() {
+	int i = Random.Range(0, forms.Length);
+	// Spawn Group at current Position
+	Instantiate(forms[i]);
+	Debug.Log("Spawend Form");
+	}
 }
